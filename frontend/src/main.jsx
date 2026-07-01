268fd6ef089839f88a2f91f84ec952d74d75202b
Aishwaryabhavaraju <aishwaryabhavaraju11@gmail.com>
2026-07-01 16:59:18 +0530

    feat: configure global toast notifications

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"
+      />
+    </BrowserRouter>
+  </React.StrictMode>
 );
